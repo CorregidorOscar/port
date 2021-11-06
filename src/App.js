8b17c8bbee72a0216fa78044c9478779af8fbfb1
CorregidorOscar <corregidoroscar18@gmail.com>
2021-11-06 19:09:09 -0300

    oscar modificado

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Rama Oscar
+          Rama Oscar modificado
         </a>
       </header>
     </div>
