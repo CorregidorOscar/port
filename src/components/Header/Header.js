c1500a6578bf38c903664567ee623c89f7f4a2f8
CorregidorOscar <corregidoroscar18@gmail.com>
2021-11-06 23:19:22 -0300

    esqueleto Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,25 @@
 export default function Header() {
   return (
-    <div className="contenedor">
-      <div className="contenedor-foto">
+    <div className="container">
+      <div className="container__image">
         <img
           src="https://previews.123rf.com/images/jemastock/jemastock1707/jemastock170704337/81629882-retrato-persona-joven-estudiante-persona-ilustración-vectorial.jpg"
           alt="retrato"
         />
       </div>
-      <div className="contenedor-info"></div>
+      <div className="container__info">
+        <span>Welcome to my World</span>
+        <h1>
+          Hi, I’m Jone Doe
+          <br />
+          <ul class="container__info__list">
+            <li class="container__info__list__item">UX Designer.</li>
+            <li class="container__info__list__item">Content Writter</li>
+            <li class="container__info__list__item">UI Designer.</li>
+          </ul>
+        </h1>
+        <h2>based in Cafayate</h2>
+      </div>
     </div>
   );
 }
