20023eeadaa2b6ea04f9bded99fe92ecf4caebf8
CorregidorOscar <corregidoroscar18@gmail.com>
2021-11-07 02:36:02 -0300

    Header quasiComplete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,32 @@
+import styles from "./Header.module.css";
 export default function Header() {
   return (
-    <div className="container">
-      <div className="container__image">
+    <div className={styles.container}>
+      <div className={styles.container__image}>
         <img
+          className={styles.container__image__img}
           src="https://previews.123rf.com/images/jemastock/jemastock1707/jemastock170704337/81629882-retrato-persona-joven-estudiante-persona-ilustración-vectorial.jpg"
           alt="retrato"
         />
       </div>
-      <div className="container__info">
-        <span>Welcome to my World</span>
-        <h1>
-          Hi, I’m Jone Doe
-          <br />
-          <ul class="container__info__list">
-            <li class="container__info__list__item">UX Designer.</li>
-            <li class="container__info__list__item">Content Writter</li>
-            <li class="container__info__list__item">UI Designer.</li>
-          </ul>
-        </h1>
+      <div className={styles.container__info}>
+        <span>Bienvenido</span>
+        <h1>Hola, Soy Lorem ipsum dolor </h1>
+        <div className={styles.container__skills}>
+          <div className={styles.container__animation}>
+            <ul class={styles.container__animation__list}>
+              <li class={styles.container__animation__list__item}>
+                UX Designer.
+              </li>
+              <li class={styles.container__animation__list__item}>
+                Content Writter.
+              </li>
+              <li class={styles.container__animation__list__item}>
+                UI Designer.
+              </li>
+            </ul>
+          </div>
+        </div>
         <h2>based in Cafayate</h2>
       </div>
     </div>
