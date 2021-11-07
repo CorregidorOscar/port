333a337a03c5c56f36c65fc81d42fb331a1768e1
CorregidorOscar <corregidoroscar18@gmail.com>
2021-11-06 22:43:07 -0300

    agregado Header en app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,13 @@
-console.log("hola");
+export default function Header() {
+  return (
+    <div className="contenedor">
+      <div className="contenedor-foto">
+        <img
+          src="https://previews.123rf.com/images/jemastock/jemastock1707/jemastock170704337/81629882-retrato-persona-joven-estudiante-persona-ilustración-vectorial.jpg"
+          alt="retrato"
+        />
+      </div>
+      <div className="contenedor-info"></div>
+    </div>
+  );
+}
